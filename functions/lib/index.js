e90030051e59ddd6481eae5573b61cc39dd8d5f5
Maxwell Nelson <Max_L_Nelson@yahoo.com>
2018-03-04 11:13:12 -0600

    Configured FB function to send contact email via gmail api.

--- functions/lib/index.js
+++ functions/lib/index.js
@@ -7,11 +7,52 @@ const functions = require("firebase-functions");
 // export const helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
-const mj = require('node-mailjet')
-    .connect(process.env.MJ_API_PK, process.env.MJ_API_SK);
+const express = require('express');
+const cors = require('cors')({ origin: "*" });
+const gNode = require('gmail-node');
+const app = express();
+const st = {
+    access_token: "ya29.GltyBbwklT6aosi_ke8JwDPBkjdJkpHaYzXecXxk0otmwB35EvJR1xTlNvUCxCYnERiv-Kue3-n8evWbF_k-7auFw31OwGQTjWui8u8G-imp3-QgnUSwBKtPAl4g",
+    refresh_token: "1/b53BV28huICUcva_XFWByUv8qFfMOoDmrPJuuLQgpno",
+    token_type: "Bearer",
+    expiry_date: 1520019374269
+};
+const cs = {
+    installed: {
+        client_id: "459961063352-eftqjmc4r9l48dgatnem41unc2jkn19q.apps.googleusercontent.com",
+        project_id: "b2b-firebase-eb256",
+        auth_uri: "https://accounts.google.com/o/oauth2/auth",
+        token_uri: "https://accounts.google.com/o/oauth2/token",
+        auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
+        client_secret: "3mROTG3oUytjd3n707LXQWuL",
+        redirect_uris: [
+            "urn:ietf:wg:oauth:2.0:oob",
+            "http://localhost"
+        ]
+    }
+};
 exports.sendContactMessage =
-    functions.firestore.document('/messages/{name}').onCreate(event => {
-        console.log(event);
-        return event;
+    functions.https.onRequest((req, res) => {
+        cors(req, res, () => {
+            const mailOpts = {
+                to: "Budget2Beautiful@gmail.com",
+                subject: `You have a new message from ${req.body.name}`,
+                message: `<div>From: ${req.body.name}</div>
+                <div>Email:  <a href=${req.body.email}>${req.body.email}</a></div>
+                <div>Phone: ${req.body.phone} </div>
+                <div>Sent at: ${req.body.date}'</div>
+                <div>Message: ${req.body.message} </div>`
+            };
+            gNode.sendHTMLWithToken(mailOpts, cs, st, (err, success) => {
+                if (err) {
+                    console.log(`There was an error: ${err}`);
+                    res.status(500).send('Error sending email.');
+                }
+                if (success) {
+                    console.log("Email has been sent successfully.");
+                    res.status(200).send(req.body);
+                }
+            });
+        });
     });
 //# sourceMappingURL=index.js.map
--- server.js
+++ server.js
@@ -2,8 +2,29 @@ const express = require('express');
 const path = require('path');
 const http = require('http');
 const bodyParser = require('body-parser');
+const gNode = require('gmail-node');
 
 const app = express()
+
+
+const cs = {
+  installed:
+    {client_id:"459961063352-eftqjmc4r9l48dgatnem41unc2jkn19q.apps.googleusercontent.com",
+    project_id:"b2b-firebase-eb256",
+    auth_uri:"https://accounts.google.com/o/oauth2/auth",
+    token_uri:"https://accounts.google.com/o/oauth2/token",
+    auth_provider_x509_cert_url:"https://www.googleapis.com/oauth2/v1/certs",
+    client_secret:"3mROTG3oUytjd3n707LXQWuL",
+    redirect_uris: [
+      "urn:ietf:wg:oauth:2.0:oob",
+      "http://localhost"
+    ]}
+}
+
+gNode.init(cs, './token.json', (err, suc) => {
+  if(err) return err;
+  console.log(suc);
+})
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
